349c687a5c89e70ab4898b612eb433362423300a
conta idriss <idriss1elouiri2@gmail.com>
2024-07-18 21:48:20 +0100

    change my app to react app

--- client/src/app/register/page.jsx
+++
@@ -1,8 +0,0 @@
-import RegisterComp from "@/components/register/Register";
-import React from "react";
-
-const Register = () => {
-  return <RegisterComp />;
-};
-
-export default Register;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,26 +1,11 @@
-const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/flowbite-react/**/*.{js,jsx,mdx}',
-    flowbite.content(),
-
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    flowbite.plugin(),
-
-  ],
-  
-};
+  plugins: [],
+}
