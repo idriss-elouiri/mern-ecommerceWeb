d1ed92cf34c2b9ebbd93d20e1cd80159297e3490
conta idriss <idriss1elouiri2@gmail.com>
2024-07-19 14:40:30 +0100

    create a dashboard and dashProfile ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import PrivateRoute from './components/PrivateRoute';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
@@ -10,6 +12,9 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
         <Route path='/log-in' element={<Login />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
