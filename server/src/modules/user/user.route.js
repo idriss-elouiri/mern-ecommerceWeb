4fd541e1cbc1c51f59632cdc80c62e83144b3148
conta idriss <idriss1elouiri2@gmail.com>
2024-07-21 20:30:38 +0100

    add getUsers and get only one user api route

--- server/src/modules/user/user.route.js
+++ server/src/modules/user/user.route.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.put('/update/:userId', verifyToken, userController.updateUser);
 router.delete('/delete/:userId', verifyToken, userController.deleteUser);
 router.post('/signout', userController.signout);
+router.get('/getusers', verifyToken, userController.getUsers);
+router.get('/:userId', userController.getUser);
 
 
 export default router;
