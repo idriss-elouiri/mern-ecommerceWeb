d73e96b61c5e9db336b53009cabe8f0522651dfd
conta idriss <idriss1elouiri2@gmail.com>
2024-08-03 19:05:33 +0100

    save changes

--- server/src/index.js
+++ server/src/index.js
@@ -22,8 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Get the directory name of the current module file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // Use CORS middleware
 app.use(
@@ -34,7 +33,11 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.get("/test", (req, res) => {
   res.json({ message: "test success" });
