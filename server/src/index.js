c0c7234cd1db1afdba7c91387d1ec04f8fa8835e
conta idriss <idriss1elouiri2@gmail.com>
2024-09-04 12:16:42 +0100

    save changes

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL, 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
