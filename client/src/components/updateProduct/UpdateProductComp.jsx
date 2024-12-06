694418da105002ba47c72d0d886b90d6596a7690
conta idriss <idriss1elouiri2@gmail.com>
2024-12-06 09:58:05 +0100

    add changes in frontend and backend

--- client/src/components/updateProduct/UpdateProductComp.jsx
+++ client/src/components/updateProduct/UpdateProductComp.jsx
@@ -169,67 +169,184 @@ export default function UpdatedProductComp() {
     }
   };
 
-  const propertiesToFill = categories.reduce((acc, cat) => {
-    if (cat._id === formData.category) {
-      acc.push(...cat.properties);
-      let parentCat = cat.parent;
-      while (parentCat?._id) {
-        const parent = categories.find(({ _id }) => _id === parentCat._id);
-        if (parent) {
-          acc.push(...parent.properties);
-          parentCat = parent.parent;
-        } else {
-          break;
-        }
-      }
-    }
-    return acc;
-  }, []);
+  const handleFormChange = (e) => {
+    const { id, value } = e.target;
 
-  const setProductProp = (propName, value) => {
-    setProductProperties((prev) => ({ ...prev, [propName]: value }));
+    const updatedValue = id === "price" ? parseFloat(value) : value;
+
+    setFormData((prevData) => ({ ...prevData, [id]: updatedValue }));
   };
 
-  const updateImagesOrder = (imageUrls) => {
-    setImages(imageUrls);
+  const propertiesToFill = () => {
+    const props = [];
+    if (categories.length > 0 && formData.category) {
+      let catInfo = categories.find(({ _id }) => _id === formData.category);
+      props.push(...catInfo.properties);
+      while (catInfo?.parent?._id) {
+        const parentCat = categories.find(
+          ({ _id }) => _id === catInfo.parent._id
+        );
+        props.push(...parentCat.properties);
+        catInfo = parentCat;
+      }
+    }
+    return props;
+  };
+
+  const setProductProp = (propName, value) => {
+    setProductProperties((prevProps) => ({ ...prevProps, [propName]: value }));
   };
 
   return (
     <>
-      <div className="flex flex-col md:flex-row">
-        <div className="p-7 border-b md:border-r md:min-h-screen border-gray-500">
-          <FilterForm
-            sidebarData={sidebarData}
-            handleChange={handleChange}
-            categories={categories}
-            handleSubmit={handleSubmit}
-          />
-        </div>
-        <div className="w-full">
-          <h1 className="text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5">
-            نتائج المنتجات:
-          </h1>
-          <div className="p-7 flex flex-wrap gap-4">
-            {loading && (
-              <p className="text-xl text-gray-500">جاري التحميل...</p>
-            )}
-            {!loading && products.length === 0 && (
-              <p className="text-xl text-gray-500">لم يتم العثور على منتجات.</p>
-            )}
-            {!loading &&
-              products.map((productInfo) => (
-                <ProductBox key={productInfo._id} productInfo={productInfo} />
+      <div className="p-3 max-w-3xl mx-auto min-h-screen">
+        <h1 className="text-center text-3xl my-7 font-semibold">إنشاء منتج</h1>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4 sm:flex-row justify-between">
+            <TextInput
+              type="text"
+              placeholder="العنوان"
+              required
+              id="title"
+              className="flex-1"
+              onChange={handleFormChange}
+              value={formData.title}
+            />
+            <Select
+              id="category"
+              value={formData.category}
+              onChange={handleFormChange}
+            >
+              <option value="0">لم يتم اختيار فئة</option>
+              {categories.map((cat) => (
+                <option key={cat._id} value={cat._id}>
+                  {cat.name}
+                </option>
               ))}
-            {showMore && (
-              <button
-                onClick={handleShowMore}
-                className="text-teal-500 text-lg hover:underline p-7 w-full"
-              >
-                عرض المزيد
-              </button>
-            )}
+            </Select>
+            {productProperties &&
+              propertiesToFill().map((p) => (
+                <div key={p.propertyName} className="flex items-center gap-2">
+                  <label>
+                    {p.propertyName
+                      .replace(/_/g, " ") // استبدال الشرطات السفلية بمسافات
+                      .replace(/\b\w/g, (c) => c.toUpperCase())}{" "}
+                    {/* تحويل أول حرف لكل كلمة إلى حرف كبير */}
+                  </label>
+                  <Select
+                    value={productProperties[p.propertyName] || ""}
+                    onChange={(ev) =>
+                      setProductProp(p.propertyName, ev.target.value)
+                    }
+                  >
+                    {p.values.map((v) => (
+                      <option key={v} value={v}>
+                        {v}
+                      </option>
+                    ))}
+                  </Select>
+                </div>
+              ))}
           </div>
-        </div>
+          <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
+            <FileInput
+              type="file"
+              accept="image/*"
+              id="images"
+              multiple
+              onChange={(e) => setFiles(e.target.files)}
+            />
+            <Button
+              type="button"
+              gradientDuoTone="purpleToBlue"
+              size="sm"
+              outline
+              onClick={handleImageSubmit}
+              disabled={uploading}
+            >
+              {imageUploadProgress ? (
+                <div className="w-16 h-16">
+                  <CircularProgressbar
+                    value={imageUploadProgress}
+                    text={`${imageUploadProgress || 0}%`}
+                  />
+                </div>
+              ) : (
+                "رفع الصورة"
+              )}
+            </Button>
+          </div>
+          {imageUploadError && (
+            <Alert color="failure">{imageUploadError}</Alert>
+          )}
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mb-2">
+            <ReactSortable
+              list={images}
+              setList={setImages}
+              className="w-[350px] h-auto gap-2 flex justify-between align-items-center"
+            >
+              {images.map((link, index) => (
+                <div key={link} className="relative group">
+                  <img
+                    src={link}
+                    alt="منتج"
+                    className="object-cover h-32 w-44 rounded-md border p-2 cursor-pointer transition-transform transform-gpu group-hover:scale-105"
+                  />
+                  <button
+                    className="absolute top-2 right-2 cursor-pointer opacity-0 group-hover:opacity-100"
+                    onClick={() => handleRemoveImage(index)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 text-red-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              ))}
+            </ReactSortable>
+          </div>
+          <Textarea
+            id="briefDesc"
+            placeholder="وصف مختصر"
+            required
+            onChange={handleFormChange}
+            value={formData.briefDesc}
+          />
+          <ReactQuill
+            ref={quillRef}
+            theme="snow"
+            onChange={(value) =>
+              setFormData((prev) => ({ ...prev, content: value }))
+            }
+            value={formData.content}
+            placeholder="المحتوى"
+          />
+          <TextInput
+            type="number"
+            placeholder="price"
+            required
+            id="price"
+            className="flex-1"
+            onChange={handleFormChange}
+            value={formData.price}
+          />
+          <div className="flex justify-end mt-3">
+            <Button type="submit" gradientDuoTone="purpleToBlue">
+              إنشاء المنتج
+            </Button>
+          </div>
+          {publishError && <Alert color="failure">{publishError}</Alert>}
+        </form>
       </div>
     </>
   );
